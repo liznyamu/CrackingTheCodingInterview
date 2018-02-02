c9cc4f53ae33c7f76532755e25bc75acb10a86a9
liznyamu <liznyamu@gmail.com>
2018-02-02 10:34:55 +0300

    Added Data Objects - PartialResult and PartialSum

---
+++ Java/Chapter 2 Linked List/Q2_05_Sum_Lists/PartialResults.java
@@ -0,0 +1,13 @@
+package Q2_05_Sum_Lists;
+
+import CtCLibrary_erza.LinkedListNode;
+
+public class PartialResults {
+
+	// partial Sum
+	LinkedListNode pSum = null;
+
+	// carryOne on partial Sum 0 - no, 1 - yes
+	int carryOne = 0;
+
+}
